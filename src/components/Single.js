8697b39afd8c343c5662112a68f13466567465e3
imuhammadshoaib <muhammadshoaib.me@gmail.com>
2022-07-25 03:08:47 +0500

    init

---
+++ src/components/Single.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import Axios from "axios";
+import { useEffect , useState } from "react";
+const Single = () => {
+    let {surahid} = useParams();
+    const [ayah, setAyah] = useState([]);
+    const [surahName, setSurahName] = useState("");
+    const [surahNameEng, setSurahNameEng] = useState("");
+
+    // API Endpoint
+    const getSingleAyah = `http://api.alquran.cloud/v1/surah/${surahid}`;
+    
+    // API Request
+    const fetchAyah = async () =>{
+        
+        var getResult = await Axios.get(getSingleAyah);
+        setAyah(getResult.data.data.ayahs);
+        setSurahName(getResult.data.data.name);
+        setSurahNameEng(getResult.data.data.englishNameTranslation);
+        
+        console.log(getResult); 
+
+    }
+
+    // Component Did Mount
+      useEffect(() => {
+        fetchAyah();
+    }, []);
+    return(
+        <>
+        <div className="container mb-5">
+            <div className="row justify-content-center g-0">
+                <div className="col-md-10  border border-dark rounded shadow-lg">
+                    <div className="row g-0 border-bottom border-dark shadow-sm">
+                    <div className="col-md-3"></div>
+                    <div className="col-6 ">
+                        <p className="fs-1 fw-bold text-center lh-base text-end mt-3 mb-0">{surahName}</p>
+                        {/* <p className="fs-3 fw-normal text-center lh-base text-end ">{surahNameEng}</p> */}
+                    </div>
+                    <div className="col-md-3"></div>
+                    </div>
+                    <div className="row justify-content-center">
+                        <div className="col-md-10 p-4">
+                            <p className="fs-3 fw-bold lh-base text-end">
+                                { ayah.map((value , index) => {
+                                    var arabic = value.numberInSurah.toLocaleString('ar-EG');
+                                    if(value.sajda == true){
+                                        var sajda = 'سجدة';
+                                    }
+                                    return(
+                                        <>
+                                        <p className="fs-1 fw-normal lh-base text-end mb-5" key = {index.text}>{value.text}
+                                            <span className="fs-2 fw-normal  m-3 text-center text-primary text-danger cstm-num"> {arabic}</span>
+                                            <span className="fs-3 fw-light text-primary">{sajda}</span>
+                                        </p>  
+            
+                                        </>              
+                                    )
+                                    })
+                                }
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+        </div>
+        </>
+    )
+ }
+
+ export default Single;
---
+++ src/components/Surah.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Axios from "axios";
+import { useEffect , useState } from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+
+const Surah = () =>{
+
+    const [verse, setVerse] = useState([]);
+
+    // API Endpoint
+    const surah = `http://api.alquran.cloud/v1/surah`;
+    
+    // API Request
+    const fetchIndoPak = async () =>{
+        
+        var getResult = await Axios.get(surah);
+        setVerse(getResult.data.data);
+        console.log(getResult); 
+
+    }
+
+    // Component Did Mount
+      useEffect(() => {
+        fetchIndoPak();
+    }, []);
+return(
+    <>
+            <Navbar/>
+            { verse.map((value , index) => {
+               return(
+                <div className="col-md-4 pe-md-0">
+                    <Link to={'/single/'+value.number} className="row g-0 border p-3 shadow-sm rounded my-2 text-dark text-decoration-none">
+                    <div className="col-6 px-1">
+                            <p className="text-end m-0 fs-4 fw-bold lh-base" key = {index.number}>{value.name}</p>
+                            <p className="text-end m-0 fs-6 fw-light lh-base" key = {index.number}>{value.numberOfAyahs+'Ayahs'}</p>
+                        </div>
+                        <div className="col-6 px-1">
+                            <p className="text-start m-0 fs-5 fw-bold lh-base" key = {index.number}>{value.englishName}</p>
+                            <p className="text-start m-0 fs-6 fw-light pt-1 lh-base" key = {index.number}>{value.englishNameTranslation}</p>                            
+                        </div>
+                    </Link>
+
+                </div>
+               )
+                }) 
+            }
+
+    </>
+);
+}
+export default Surah;
